07b0ed0d2fb3d7976ad38e4e21866b265d3cb57c
Akito <1161780477@qq.com>
2023-12-20 01:15:04 +0800

    1.完成了界面的UI搭建 2.CSS完成 3.开发JS部分开始

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,19 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+"use strict";
+
+const api_key = "b6c3dfec0d0c35638794a33149e183c7";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback function
+ */
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
